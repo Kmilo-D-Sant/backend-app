9e820cad04274d474adc9acb15f8c60970d7585c
Leandro Llorente <lllorente@opusbehavioral.com>
2024-06-15 04:19:38 -0400

    Arreglo de error en recibirDatos

--- api/models.py
+++ api/models.py
@@ -11,20 +11,22 @@ from .helpers import crearSerializador
 
 
 class Usuario(AbstractUser):
-    telefono = models.CharField(max_length=10,blank= True, null= True)
-    es_proveedor = models.BooleanField("Es proveedor", default= False)
-    imagen = models.ImageField(blank= True, null= True, default="",upload_to="imagenes/")
-    imagenB64 = models.TextField(blank= True, null= True)
-    configuracionActualizada = models.BooleanField("Configuración actual", default= False)
-    
+    telefono = models.CharField(max_length=10, blank=True, null=True)
+    es_proveedor = models.BooleanField("Es proveedor", default=False)
+    imagen = models.ImageField(
+        blank=True, null=True, default="", upload_to="imagenes/")
+    imagenB64 = models.TextField(blank=True, null=True)
+    configuracionActualizada = models.BooleanField(
+        "Configuración actual", default=False)
 
     def __nuevo__(username, password, first_name, email, telefono, es_proveedor, is_staff, imagen):
         self = Usuario()
         if imagen != None:
             imagen_io = io.BytesIO()
             imagen.save(imagen_io, format='PNG')
-            self.imagen = InMemoryUploadedFile(imagen_io, field_name=None, name= username + ".png", content_type='imagen/png', size=imagen_io.tell, charset=None)
-            
+            self.imagen = InMemoryUploadedFile(
+                imagen_io, field_name=None, name=username + ".png", content_type='imagen/png', size=imagen_io.tell, charset=None)
+
         self.username = username
         self.set_password(password)
         self.first_name = first_name
@@ -34,13 +36,14 @@ class Usuario(AbstractUser):
         self.is_staff = is_staff
         self.imagenB64 = imagenB64
         self.save()
-        return self      
-    
+        return self
+
     def __actualizar__(self, username, password, first_name, email, telefono, es_proveedor, is_staff, imagen, imagenB64):
         if imagen != None:
             imagen_io = io.BytesIO()
             imagen.save(imagen_io, format='PNG')
-            self.imagen = InMemoryUploadedFile(imagen_io, field_name=None, name= username + ".png", content_type='imagen/png', size=imagen_io.tell, charset=None)
+            self.imagen = InMemoryUploadedFile(
+                imagen_io, field_name=None, name=username + ".png", content_type='imagen/png', size=imagen_io.tell, charset=None)
         self.username = username
         self.set_password(password)
         self.first_name = first_name
@@ -50,13 +53,13 @@ class Usuario(AbstractUser):
         self.es_proveedor = es_proveedor
         self.is_staff = is_staff
         self.save()
-        return self     
-     
+        return self
+
     def __inhabilitar__(self):
         self.is_active = False
         self.save()
-        return self 
-    
+        return self
+
     class Meta:
         verbose_name = "usuario"
         verbose_name_plural = "usuarios"
@@ -70,10 +73,10 @@ class Jugada(models.Model):
     montoCorrido = models.FloatField(default=0)
     enElBote = models.BooleanField(default=False)
     fecha = models.BigIntegerField(default=0)
-    tiroManana =models.BooleanField(default=True)
+    tiroManana = models.BooleanField(default=True)
     premio = models.FloatField(default=0)
-    enviado =models.BooleanField(default=False)
-    
+    enviado = models.BooleanField(default=False)
+
     def __nueva__(data):
         serializador = crearSerializador(Jugada, 0)
         serializer = serializador(data=data)
@@ -82,7 +85,7 @@ class Jugada(models.Model):
             return objectAux
         else:
             return None
-        
+
     # def __nueva__( tipo,  numeros,  monto,  enElBote,  corrido,  usuario, fecha, tiroManana):
     #     self =  Jugada()
     #     self.tipo = tipo
@@ -96,15 +99,15 @@ class Jugada(models.Model):
     #     self.tiroManana = tiroManana
     #     self.save()
     #     return self
-        
+
 
 class Configuracion(models.Model):
-    usuario = models.CharField( max_length=100)
+    usuario = models.CharField(max_length=100)
     fecha = models.BigIntegerField(default=0)
-    horaAperturaDia = models.CharField( max_length=20, default= "06:00:00")
-    horaCierreDia = models.CharField( max_length=20,  default= "13:15:00")
-    horaAperturaNoche = models.CharField( max_length=20, default= "14:00:00")
-    horaCierreNoche = models.CharField( max_length=20, default= "21:20:00")
+    horaAperturaDia = models.CharField(max_length=20, default="06:00:00")
+    horaCierreDia = models.CharField(max_length=20,  default="13:15:00")
+    horaAperturaNoche = models.CharField(max_length=20, default="14:00:00")
+    horaCierreNoche = models.CharField(max_length=20, default="21:20:00")
     beneficioCliente = models.FloatField(default=0.3)
     beneficioClienteBote = models.FloatField(default=0.1)
     beneficioClienteBola = models.FloatField(default=0.2)
@@ -136,38 +139,54 @@ class Configuracion(models.Model):
     topeCentena = models.IntegerField(default=1000)
     topeCentenaBote = models.IntegerField(default=200)
     topeCentenaUsuario = models.IntegerField(default=100)
-    topeCentenaBoteUsuario =models.IntegerField(default=200) 
+    topeCentenaBoteUsuario = models.IntegerField(default=200)
     topeParlet = models.IntegerField(default=10)
     topeParletBote = models.IntegerField(default=20)
     topeParletUsuario = models.IntegerField(default=10)
     topeParletBoteUsuario = models.IntegerField(default=20)
     tiroManana = models.BooleanField(default=True)
-    limitadosBola = models.CharField( max_length=100, blank=True, null=True)
-    limitadosBolaBote = models.CharField( max_length=100, blank=True, null=True)
-    limitadosBolaUsuario = models.CharField( max_length=100, blank=True, null=True)
-    limitadosBolaBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    limitadosParlet = models.CharField( max_length=100, blank=True, null=True)
-    limitadosParletUsuario = models.CharField( max_length=100, blank=True, null=True)
-    limitadosParletBote = models.CharField( max_length=100, blank=True, null=True)
-    limitadosParletBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    limitadosCentena = models.CharField( max_length=100, blank=True, null=True)
-    limitadosCentenaUsuario = models.CharField( max_length=100, blank=True, null=True)
-    limitadosCentenaBote = models.CharField( max_length=100, blank=True, null=True)
-    limitadosCentenaBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaBola = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaBolaBote = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaBolaUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaBolaBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaParlet = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaParletBote = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaParletUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaParletBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaCentena = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaCentenaBote = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaCentenaUsuario = models.CharField( max_length=100, blank=True, null=True)
-    noSeJuegaCentenaBoteUsuario = models.CharField( max_length=100, blank=True, null=True)
-    enviado =models.BooleanField(default=False)
-    
+    limitadosBola = models.CharField(max_length=100, blank=True, null=True)
+    limitadosBolaBote = models.CharField(max_length=100, blank=True, null=True)
+    limitadosBolaUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosBolaBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosParlet = models.CharField(max_length=100, blank=True, null=True)
+    limitadosParletUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosParletBote = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosParletBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosCentena = models.CharField(max_length=100, blank=True, null=True)
+    limitadosCentenaUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosCentenaBote = models.CharField(
+        max_length=100, blank=True, null=True)
+    limitadosCentenaBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaBola = models.CharField(max_length=100, blank=True, null=True)
+    noSeJuegaBolaBote = models.CharField(max_length=100, blank=True, null=True)
+    noSeJuegaBolaUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaBolaBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaParlet = models.CharField(max_length=100, blank=True, null=True)
+    noSeJuegaParletBote = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaParletUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaParletBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaCentena = models.CharField(max_length=100, blank=True, null=True)
+    noSeJuegaCentenaBote = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaCentenaUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    noSeJuegaCentenaBoteUsuario = models.CharField(
+        max_length=100, blank=True, null=True)
+    enviado = models.BooleanField(default=False)
+
     def __nueva__(data):
         serializador = crearSerializador(Configuracion, 0)
         serializer = serializador(data=data)
@@ -176,4 +195,3 @@ class Configuracion(models.Model):
             return objectAux
         else:
             return None
-
--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from .helpers import obtenerUsuario, respuesta, error, crearSerializador
 from .const import *
 from .FCMManger import sendPush
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class GestionarUsuario(View):
 
@@ -19,7 +20,8 @@ class GestionarUsuario(View):
         try:
             dataRequest = JSONParser().parse(request)
             serializador = crearSerializador(Usuario, 0)
-            nuevoUsuario =  serializador(Usuario.__nuevo__(dataRequest['username'],dataRequest['password'],dataRequest['first_name'],dataRequest['email'],dataRequest['telefono'],dataRequest['es_proveedor'],dataRequest['is_staff'], imagen), many = False).data
+            nuevoUsuario = serializador(Usuario.__nuevo__(dataRequest['username'], dataRequest['password'], dataRequest['first_name'],
+                                        dataRequest['email'], dataRequest['telefono'], dataRequest['es_proveedor'], dataRequest['is_staff'], imagen), many=False).data
             return respuesta(nuevoUsuario)
         except BaseException as err:
             return error(str(err))
@@ -30,7 +32,7 @@ class GestionarUsuario(View):
             if usuario == None or not usuario.is_active:
                 return error(MENSAJE_USUARIO_NO_AUTENTICADO, status.HTTP_403_FORBIDDEN)
             serializador = crearSerializador(Usuario, 0)
-            return respuesta(serializador(Usuario.objects.filter(id = usuario.id), many = True).data)
+            return respuesta(serializador(Usuario.objects.filter(id=usuario.id), many=True).data)
             # usuario = obtenerUsuario(request)
             # if usuario == None:
             #     return error(MENSAJE_USUARIO_NO_AUTENTICADO, status.HTTP_403_FORBIDDEN)
@@ -40,7 +42,7 @@ class GestionarUsuario(View):
             #     usuarios = serializador(Usuario.objects.filter(id = datosRequest.get('id')), many = True).data
             # else:
             #     usuarios = serializador(Usuario.objects.all(), many = True).data
-            #return respuesta(usuarios)
+            # return respuesta(usuarios)
         except BaseException as err:
             return error(str(err))
 
@@ -52,7 +54,8 @@ class GestionarUsuario(View):
             dataRequest = JSONParser().parse(request)
             imagen = Base64Aimagen(dataRequest['imagen'])
             serializador = crearSerializador(Usuario, 0)
-            usuarioActualizado =  serializador(Usuario.__actualizar__(usuario,dataRequest['username'],dataRequest['password'],dataRequest['first_name'],dataRequest['email'],dataRequest['telefono'],dataRequest['es_proveedor'],dataRequest['is_staff'], imagen, dataRequest['imagenB64']), many = False).data
+            usuarioActualizado = serializador(Usuario.__actualizar__(usuario, dataRequest['username'], dataRequest['password'], dataRequest['first_name'], dataRequest[
+                                              'email'], dataRequest['telefono'], dataRequest['es_proveedor'], dataRequest['is_staff'], imagen, dataRequest['imagenB64']), many=False).data
             return respuesta(usuarioActualizado)
         except BaseException as err:
             return error(str(err))
@@ -63,10 +66,12 @@ class GestionarUsuario(View):
             if usuario == None or not usuario.is_active:
                 return error(MENSAJE_USUARIO_NO_AUTENTICADO, status.HTTP_403_FORBIDDEN)
             serializador = crearSerializador(Usuario, 0)
-            usuarioInhabilitado =  serializador(Usuario.__inhabilitar__(usuario), many = False).data
+            usuarioInhabilitado = serializador(
+                Usuario.__inhabilitar__(usuario), many=False).data
             return respuesta(usuarioInhabilitado)
         except BaseException as err:
             return error(str(err))
+
 
 @csrf_exempt
 def recibirDatos(request):
@@ -82,16 +87,18 @@ def recibirDatos(request):
             jugadaAux = dataRequest['listaJugadas'][0]
             serializer = crearSerializador(Jugada, 0)
             fecha = str(jugadaAux["fecha"])[0:8]
-            existe = Jugada.objects.filter(Q(usuario = jugadaAux["usuario"]) and Q(tiroManana = jugadaAux["tiroManana"]) and Q(fecha__startswith = fecha))
-            if existe[0]:
+            existe = Jugada.objects.filter(Q(usuario=jugadaAux["usuario"]) and Q(
+                tiroManana=jugadaAux["tiroManana"]) and Q(fecha__startswith=fecha))
+            if len(existe)>0:
                 return error("Ya este usuario subio los datos de esa sesión")
             for item in dataRequest['listaJugadas']:
-                nueva =  Jugada.__nueva__(item)
+                nueva = Jugada.__nueva__(item)
                 jugadas.append(nueva)
             if not usuario.configuracionActualizada:
                 serializer = crearSerializador(Configuracion, 0)
                 try:
-                    confGlobal = serializer(Configuracion.objects.get(usuario="global"), many = False).data
+                    confGlobal = serializer(Configuracion.objects.get(
+                        usuario="global"), many=False).data
                     usuario.configuracionActualizada = True
                     usuario.save()
                     return respuesta(confGlobal)
@@ -100,6 +107,7 @@ def recibirDatos(request):
             return respuesta("Datos recibidos exitosamente")
     except BaseException as err:
         return error(str(err))
+
 
 @csrf_exempt
 def enviarDatos(request):
@@ -110,13 +118,16 @@ def enviarDatos(request):
                 return error(MENSAJE_USUARIO_NO_AUTENTICADO, status.HTTP_403_FORBIDDEN)
             serializadorC = crearSerializador(Configuracion, 0)
             serializadorJ = crearSerializador(Jugada, 0)
-            dataC = serializadorC( Configuracion.objects.filter(enviado=False), many = True).data
-            dataJ = serializadorJ(Jugada.objects.filter(enviado=False), many = True).data
+            dataC = serializadorC(Configuracion.objects.filter(
+                enviado=False), many=True).data
+            dataJ = serializadorJ(Jugada.objects.filter(
+                enviado=False), many=True).data
             Configuracion.objects.filter(enviado=False).update(enviado=True)
             Jugada.objects.filter(enviado=False).update(enviado=True)
-            return respuesta({"configuraciones": dataC,"jugadas":dataJ})
+            return respuesta({"configuraciones": dataC, "jugadas": dataJ})
     except BaseException as err:
         return error(str(err))
+
 
 @csrf_exempt
 def recibirConfiguracionGlobal(request):
@@ -129,11 +140,12 @@ def recibirConfiguracionGlobal(request):
             Configuracion.objects.filter(usuario="global").delete()
             datos = dataRequest['datos']['configuracion']
             datos["usuario"] = "global"
-            nuevaC =  Configuracion.__nueva__(datos)
-            Usuario.objects.all().update(configuracionActualizada = False)
+            nuevaC = Configuracion.__nueva__(datos)
+            Usuario.objects.all().update(configuracionActualizada=False)
             return respuesta("OK")
     except BaseException as err:
         return error(str(err))
+
 
 class CustomAuthToken(ObtainAuthToken):
 
@@ -147,8 +159,10 @@ class CustomAuthToken(ObtainAuthToken):
             'token': token.key,
         })
 
+
 def bienvenido(request):
     return respuesta("Bienvenido!!!")
+
 
 @csrf_exempt
 def notificar(request):
